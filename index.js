aa7b9c7abf0d87428864533d53d163f2731b8c20
Simon Ip <waterloo178@yahoo.com>
2018-04-10 00:14:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function happyHolidays() {
   return "Happy holidays!";
   } 
   
-  //var text = happyHolidays;
-  //console.log(text);
-  
-  function happyHolidaysTo (name) {
+ function happyHolidaysTo (name) {
     return "Happy holidays, " + name +"!";
   }
   
+  function happyHolidayTo (holiday, name) {
+    return "Happy " + holiday +", " + name + "!";
+  }
